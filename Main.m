2a6819fa03e4d6ac5aebe9dfc79f78f7d58ec2a0
hantyou <daniel.zhai@yahoo.com>
2021-08-25 18:43:39 +0200

    some comment added to make codes more readable

--- Main.m
+++ Main.m
@@ -6,23 +6,22 @@ reg_x=[-5,5]; % These are x and y limite for the region
 reg_y=[-5,5];
 
 
-Kernels=[(reg_x(2)-reg_x(1))*rand(m,1)+reg_x(1) (reg_y(2)-reg_y(1))*rand(m,1)+reg_y(1)]; % This contains the positions of the RBFs
-% Kernels=[kron(linspace(reg_x(1),reg_x(2),5),[1 1 1 1 1])',repmat(linspace(reg_y(1),reg_y(2),5),1,5)'];
+% Kernels=[(reg_x(2)-reg_x(1))*rand(m,1)+reg_x(1) (reg_y(2)-reg_y(1))*rand(m,1)+reg_y(1)]; % This contains the positions of the RBFs
+Kernels=[kron(linspace(reg_x(1),reg_x(2),5)/1.1,[1 1 1 1 1])',repmat(linspace(reg_y(1),reg_y(2),5)/1.1,1,5)'];
 % Gamma=2*abs(randn(m,1)); % Gamma in the RBFs
 Gamma=0.1*ones(m,1);
 % sigma=2*abs(randn(m,1)); % sigma in the RBFs
 sigma=2*ones(m,1);
 % Theta=2*abs(randn(m,1)); % scaling factors of RBFs
-Theta=0.01*zeros(m,1);
-Theta(10)=10;
+Theta=zeros(m,1);
+Theta(19)=5; % This set a peak for the field
 % This returns the true scalar value of the whole field
 mesh_value = generate_region(reg_x,reg_y,m,Gamma,sigma,Kernels,Theta);
 
 M=size(mesh_value,1);
 [mesh_x,mesh_y]=meshgrid(linspace(reg_x(1),reg_x(2),M),flip(linspace(reg_y(1),reg_y(2),M)));
-% surf(mesh_x,mesh_y,255.*mesh_value./max(max(mesh_value)),'edgecolor', 'none');
-% image(linspace(reg_x(1),reg_x(2),M),linspace(reg_y(1),reg_y(2),M),255.*mesh_value./max(max(mesh_value)));
-image(linspace(reg_x(1),reg_x(2),M),linspace(reg_y(2),reg_y(1),M),mesh_value);
+image(linspace(reg_x(1),reg_x(2),M),linspace(reg_y(2),reg_y(1),M),mesh_value,'CDataMapping','scaled');
+title("Initialization of agents and fields")
 % view(2);
 % plot3(mesh_x,mesh_y,255.*mesh_value./max(max(mesh_value)))
 % hold on;
@@ -30,16 +29,16 @@ image(linspace(reg_x(1),reg_x(2),M),linspace(reg_y(2),reg_y(1),M),mesh_value);
 % hold off;
 %% Global parameters definition
 N=25; % Number of agents
-d=0.7; % a parameter
+d=0.6; % a parameter
 d0=1.5*d; % a parameter
 d1=3.5*d; % a parameter
 r=4*d; % The communication range of an agent
 s=1; % Sampling rate of agents
-sigma_w=0.5;
+sigma_w=1; % variance of the sampling noise
 %% Agents initialization
 t=0;
-InitialPosi=-2+randn(N,2);
-InitialPosi(InitialPosi>5)=4;
+InitialPosi=-2+randn(N,2); % Initialize the agent positions
+InitialPosi(InitialPosi>5)=4; % correct positions outside the border
 InitialPosi(InitialPosi<-5)=-4;
 Agents=agent.empty(N,0);
 for n=1:N
@@ -58,8 +57,10 @@ for n=1:N
     Agents(n).v=[0;0];
     Agents(n).gamma=0.2;
     Agents(n).k3=0.1;
-    Agents(n).k4=0.2;
-    Agents(n).delta_t=1;
+    Agents(n).k4=0.1;
+    Agents(n).delta_t=0.1;
+    Agents(n).BorderLimitX=reg_x;
+    Agents(n).BorderLimitY=reg_y;
 end
 
 hold on;
@@ -69,31 +70,32 @@ Agents=Agents.UpdateNeighbour;
 
 %% Distributed Learning
 IterationFlag=1;
-IterNumMax=2000;
+IterNumMax=1000;
 IterCount=0;
 figure(2),
+errorRecord=[];
 while IterationFlag
     IterCount=IterCount+1;
     
-    Agents=Agents.Measure(Theta,Kernels,Gamma,sigma,sigma_w);
-    Agents=Agents.ReceiveNeighbourMeasurements;
-    Agents=Agents.Learn;
-    Agents=Agents.GenerateControl;
-    Agents=Agents.Move;
+    Agents=Agents.Measure(Theta,Kernels,Gamma,sigma,sigma_w); % The agents measure their local scalar field values
+    Agents=Agents.ReceiveNeighbourMeasurements; % The agents exchange their measurement with neighbours
+    Agents=Agents.Learn; % The agents estimate their local field with the measurement
+    Agents=Agents.GenerateControl; % The control signal is generated, which will be used to move the agents
+    Agents=Agents.Move; % The agents move to new positions according to the control signal
+    Agents=Agents.UpdateNeighbour; % agents update their new list of neighbours
     
-    for i=1:N
-        Agents(i).Position(Agents(i).Position>5)=5;
-        Agents(i).Position(Agents(i).Position<-5)=-5;
+    for i=1:N % Hard decision that restrict the agents stay inside the region M of interest
+        Agents(i).Position(Agents(i).Position>=5)=4.99;
+        Agents(i).Position(Agents(i).Position<=-5)=-4.99;
     end
     
     Posi=reshape([Agents.Position],[2,N])';
     
-    Agents=Agents.UpdateNeighbour;
-    
-    if ~mod(IterCount,10)
+    % plot the current agent positions and the estimated field of agent 1
+    if ~mod(IterCount,100)
         figure(2),
         mesh_value_est_1 = generate_region(reg_x,reg_y,m,Gamma,sigma,Kernels,Agents(1).Theta_est);
-        image(linspace(reg_x(1),reg_x(2),M),linspace(reg_y(2),reg_y(1),M),mesh_value_est_1);
+        image(linspace(reg_x(1),reg_x(2),M),linspace(reg_y(2),reg_y(1),M),mesh_value_est_1,'CDataMapping','scaled');
         hold on;
         scatter(Posi(:,1),Posi(:,2),'g*');
         scatter(Agents(1).Position(1),Agents(1).Position(2),'r*');
@@ -105,7 +107,21 @@ while IterationFlag
     if IterCount>IterNumMax
         IterationFlag=0;
     end
+    errorRecord=[errorRecord ReportMSEError(Agents)];
 end
+
+figure(2),
+mesh_value_est_1 = generate_region(reg_x,reg_y,m,Gamma,sigma,Kernels,Agents(1).Theta_est);
+image(linspace(reg_x(1),reg_x(2),M),linspace(reg_y(2),reg_y(1),M),mesh_value_est_1,'CDataMapping','scaled');
+hold on;
+scatter(Posi(:,1),Posi(:,2),'g*');
+scatter(Agents(1).Position(1),Agents(1).Position(2),'r*');
+scatter(Kernels(:,1),Kernels(:,2),'ro');
+title("Final positions of agents and estimated field of agent-1")
+hold off;
+
+figure,semilogy(errorRecord);
+title("MSE - Iteration")
 
 
 
--- agent.m
+++ agent.m
@@ -1,35 +1,35 @@
 classdef agent
-    %AGENT Summary of this class goes here
-    %   Detailed explanation goes here
     
     properties
-        Code
-        CommuDist
-        Position
-        Neighbour
-        Speed
-        SampleNumber
-        RealLocalValue
-        Measurement_self
-        Measurement_around
-        K
-        P
-        d
-        d0
-        d1
-        Phi_star
-        Theta_est
-        Mu_est
-        Ys
-        Gamma
-        sigma
-        Kernels
-        v
-        gamma
-        delta_t
-        k_di
-        k3
-        k4
+        Code % agent serial number
+        BorderLimitX % X limit of the whole region
+        BorderLimitY % Y limit of the whole region
+        CommuDist % The farthest distance of communication of between two agents
+        Position % The current position of an agent
+        Neighbour % record the neighbors of an agents (not including itself, agent code from small to large)
+        Speed % This variable is useless, replaced by variable v below 
+        SampleNumber % number of samples from self and neighbors
+        RealLocalValue % The real value of local scalar field
+        Measurement_self % The measured value of an agent itself
+        Measurement_around % The collected measurements from the neighbor of an agent
+        Ys % The measured values from itself and its neighbors
+        K % a variable in the distributed learning stage (field estimation) 
+        P % a variable in the distributed learning stage (field estimation) 
+        d % a parameter related to the inner potential of the agent flock
+        d0 % a parameter related to the inner potential of the agent flock
+        d1 % a parameter related to the inner potential of the agent flock
+        Phi_star % a variable used in the distributed learning stage (field estimation) 
+        Theta_est % the estimated parameters of the field 
+        Mu_est % the estimated value of current agent position 
+        Gamma % scalar values of the radial basis functions
+        sigma % variance of the radial basis functions
+        Kernels % kernel positions of the radial basis functions
+        v % the control of the agent, can be regarded as its speed
+        gamma % a standard adaptive gain sequence, reduces as iteration number goes up
+        delta_t % the time between two update of estimation and movement 
+        k_di % a scalar of the importance of speed feedback in control stage
+        k3 % a scalar of the importance of flock speed in control stage 
+        k4 % a scalar of the importance of field gradient in control stage
     end
     
     methods
@@ -40,17 +40,31 @@ classdef agent
             end
         end
         
+        % function MEASURE gives the current measurement of the agent
         function obj=Measure(obj,theta,Kernel,Gamma,sigma,sigma_w)
             N=length(obj);
             for i=1:N
                 %                 RealValue=0;
                 Phi_star=vec_Phi(obj(i).Position,Kernel,Gamma,sigma);
                 RealValue=Phi_star*theta;
+                obj(i).RealLocalValue=RealValue;
                 MeasureValue=RealValue+sigma_w*randn;
                 obj(i).Measurement_self=MeasureValue;
             end
         end
         
+        % function REPORTMSEERROR calculate the current MSE of the agents
+        % estimation of the scalar field
+        function error=ReportMSEError(obj)
+            N=length(obj);
+            error=0;
+            for i=1:N
+                error=error+(obj(i).Mu_est-obj(i).RealLocalValue)^2;
+            end
+            error =error/N;
+        end
+        
+        % function UPDATENEIGHBOUR update the neighbours of agents
         function obj=UpdateNeighbour(obj)
             N=length(obj);
             for i=1:N
@@ -70,6 +84,8 @@ classdef agent
             end
         end
         
+        % function RECEIVENEIGHBOURMEASUREMENTS let agents collect
+        % measurement values from their neighbours
         function obj=ReceiveNeighbourMeasurements(obj)
             N=length(obj);
             for i=1:N
@@ -87,6 +103,8 @@ classdef agent
             end
         end
         
+        % function LEARN is the recursive update the estimation of the
+        % scalar field
         function obj=Learn(obj)
             N=length(obj);
             for i=1:N
@@ -111,10 +129,17 @@ classdef agent
             
         end
         
+        % function  CALCDIFFPOTENTIALU calculate the potential U used in
+        % control stage
         function DiffPotential=CalcDiffPotentialU(obj,obj_self)
             %             obj=obj_all;
             alpha=1;
+            beta=0.1;
             DiffPotential=[0,0];
+            k1=9;
+            k2=0;
+            k1=k1/(k1+k2);
+            k2=1-k1;
             for j=obj_self.Neighbour
                 r_ij=norm(obj_self.Position-obj(j).Position)^2;
                 if r_ij<(obj_self.d0^2)
@@ -123,11 +148,20 @@ classdef agent
                     DiffPotential=DiffPotential+func_rho((sqrt(r_ij)-obj_self.d0)/(abs(obj_self.d1-obj_self.d0)))*norm(obj_self.d0^2-obj_self.d^2)/(alpha+obj_self.d0^2)^2*(obj_self.Position-obj(j).Position);
                 end
             end
+            PotentialInner=DiffPotential;
+            order=2;
+            PotentialBorder1=-beta/(obj_self.Position(1)-obj_self.BorderLimitX(1))^order+beta/(obj_self.Position(1)-obj_self.BorderLimitX(2))^order;
+            PotentialBorder2=-beta/(obj_self.Position(2)-obj_self.BorderLimitY(1))^order+beta/(obj_self.Position(2)-obj_self.BorderLimitY(2))^order;
+            PotentialBorder=[PotentialBorder1,PotentialBorder2];
+            PotentialBorder(abs(PotentialBorder)>50)=PotentialBorder(abs(PotentialBorder)>10)*0.1;
+            DiffPotential=k1*PotentialInner+k2*PotentialBorder;
         end
         
+        % function GENRATECONTROL generate the control signal v for the
+        % agents
         function obj=GenerateControl(obj)
             N=length(obj);
-            k_di=1*eye(2);
+            k_di=0.5*eye(2);
             delta_t=obj(1).delta_t;
             for i=1:N
                 DiffPotential=1*obj.CalcDiffPotentialU(obj(i));
@@ -137,11 +171,13 @@ classdef agent
                 end
                 u_i=u_i+obj(i).k4*(vec__phi(obj(i).Position,obj(i).Kernels,obj(i).Gamma,obj(i).sigma)./obj(i).sigma.^2.*(-obj(i).Position+obj(i).Kernels))'*obj(i).Theta_est;
                 gamma_old=obj(i).gamma;
-                obj(i).gamma=obj(i).gamma*1/1.0001;
+                obj(i).gamma=obj(i).gamma*1/1.00001;
                 obj(i).v=obj(i).gamma/delta_t*(delta_t/gamma_old*obj(i).v+gamma_old*u_i);
             end
         end
         
+        % function MOVE let agents move to a new position according to
+        % control signal v
         function obj=Move(obj)
             N=length(obj);
             for i=1:N
